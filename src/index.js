dbf92b6dc9d0b2a19d6bca17429351e974bda8b1
jenny obryant <jennyobryant@protonmail.com>
2019-10-09 17:28:01 -0600

    add darkmode in the body of html

--- src/index.js
+++ src/index.js
@@ -14,12 +14,20 @@ const App = () => {{
 
 }
   const [coinData, setCoinData] = useState([]);
-  const [value, setValue] = useLocalStorage("foo", "hi");
+  const [darkMode, setDarkMode] = useLocalStorage("dark-mode", false);
  
+  useEffect(() => {
+      if (darkMode) {
+        console.log("SET DARK MODE");
+       
+      } else {
+        console.log("UNSET DARK MODE");
+      }
+  }, [darkMode]);
 
 
   useEffect(() => {
-    setValue ("hello")
+    setDarkMode(true);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
