c23dffff5d062d5697a9f7a51e92054f1b97aeb0
jenny obryant <jennyobryant@protonmail.com>
2019-10-08 19:18:46 -0600

    create useLocalStorage file

--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
-const App = () => {
+
+
+const App = () => {{
+
+}
   const [coinData, setCoinData] = useState([]);
+ 
+
 
   useEffect(() => {
+
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
